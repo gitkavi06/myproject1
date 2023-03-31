e8083a9925d749dcbb633fd7925832fb23cf4324
kavi <mailtorkavi@gmail.com>
2023-03-31 14:54:04 +0530

    Added new project1

--- src/main/java/com/example/myproject1/Myproject1Application.java
+++ src/main/java/com/example/myproject1/Myproject1Application.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.myproject1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Myproject1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Myproject1Application.class, args);
 	}
 
 }
--- src/test/java/com/example/myproject1/Myproject1ApplicationTests.java
+++ src/test/java/com/example/myproject1/Myproject1ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.myproject1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Myproject1ApplicationTests {
 
 	@Test
 	void contextLoads() {
